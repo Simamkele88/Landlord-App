a803a6024e38b5afbd355f2479f4215601e75ae1
Simamkele88 <nsindisosimamkele88@gmail.com>
2026-07-18 21:33:54 +0200

    changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "chihwa_rentals_db",
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "Wekeza2004",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ,
+  database: process.env.DB_NAME ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
 });
 
 module.exports = pool;
